7e8017bf7bc2fc3c1c92cfc7a2273f3b676cac0b
Christian Schaefer <chrisse.s@googlemail.com>
2019-05-24 20:32:53 +0200

    Changed message

--- main.go
+++ main.go
@@ -14,6 +14,6 @@ type PubSubMessage struct {
 
 // HelloPubSub consumes a Pub/Sub message.
 func HelloPubSub(ctx context.Context, m PubSubMessage) error {
-	log.Println(string(m.Data))
+	log.Println(string(m.Data) + ". It's me!")
 	return nil
 }
