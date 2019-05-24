9faf6e64f2c377a29b178fcb0c701c0978fe637c
Christian Schaefer <chrisse.s@googlemail.com>
2019-05-24 20:32:17 +0200

    Initial add of cloud function

---
+++ main.go
@@ -0,0 +1,19 @@
+// Package lunchorder contains a Pub/Sub Cloud Function.
+package lunchorder
+
+import (
+	"context"
+	"log"
+)
+
+// PubSubMessage is the payload of a Pub/Sub event. Please refer to the docs for
+// additional information regarding Pub/Sub events.
+type PubSubMessage struct {
+	Data []byte `json:"data"`
+}
+
+// HelloPubSub consumes a Pub/Sub message.
+func HelloPubSub(ctx context.Context, m PubSubMessage) error {
+	log.Println(string(m.Data))
+	return nil
+}
