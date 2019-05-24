2bcc87ea43d2c4fc91b4aee80cd9031b33e96e10
Christian Schaefer <chrisse.s@googlemail.com>
2019-05-24 20:39:47 +0200

    Playing around

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 // PubSubMessage is the payload of a Pub/Sub event. Please refer to the docs for
 // additional information regarding Pub/Sub events.
 type PubSubMessage struct {
-	Data []byte `json:"data"`
+	Data string `json:"name"`
 }
 
 // HelloPubSub consumes a Pub/Sub message.
 func HelloPubSub(ctx context.Context, m PubSubMessage) error {
-	log.Println(string(m.Data) + ". It's me!")
+	log.Println(string(m.Data) + ". It's me!... Again!")
 	return nil
 }
